5a1f431d18d453a654d3fe44a471a4ed07959cde
Qian Kun <kqian@dawntech-info.com>
2023-11-24 17:48:06 +0800

    feat: 【洛谷】P5710，数的性质，逻辑运算

---
+++ luogu/p5710.c
@@ -0,0 +1,48 @@
+/**
+* P5710 【深基3.例2】数的性质
+* https://www.luogu.com.cn/problem/P5710
+*/
+#include <stdio.h>
+
+/**
+* 性质1是否成立
+* @param x 输入的整数
+* @return {int} 1成立，0不成立
+*/
+int prop1(int x) {
+    return x % 2 == 0 ? 1 : 0;
+}
+
+/**
+* 性质2是否成立
+* @param x 输入的整数
+* @return {int} 1成立，0不成立
+*/
+int prop2(int x) {
+    return 4 < x && x <= 12 ? 1 : 0;
+}
+
+int func() {
+    // 输入一个正整数
+    int x;
+    scanf("%d", &x);
+
+    // 对这个数进行条件判断，成立则1，不成立则0
+    // 小A 同时成立 逻辑与
+    int a = prop1(x) && prop2(x) ? 1 : 0;
+    // Uim 至少一个成立 逻辑或
+    int uim = prop1(x) || prop2(x) ? 1 : 0;
+    // 八尾 刚好一个 位异或
+    int bawei = prop1(x) ^ prop2(x);
+    // 正妹 两者都不 逻辑非+逻辑与
+    int zhengmei = !prop1(x) && !prop2(x) ? 1 : 0;
+
+    // 输出四个人是否喜欢的结果
+    printf("%d %d %d %d\n", a, uim, bawei, zhengmei);
+    return 0;
+}
+
+int main() {
+    func();
+    return 0;
+}
--- main.c
+++ main.c
@@ -1,3 +1,7 @@
+/**
+* 题目标题 main.c是一个快速模板
+* 题目链接 https://abc.com
+*/
 #include <stdio.h>
 
 int main() {
